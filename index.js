b062d6c71358bfa79c81ad8751ee8173d56dc96e
unknown <sumaiya.rrashid@gmail.comm>
2023-05-02 22:23:32 +0200

    chefReciepes add

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors());
 const chefsProfiles = require('./data/chefprofile.json')
-
+const recipes = require('./data/chefsRecipes.json')
+app.get('/recipes', (req,res)=>{
+    res.send(recipes)
+})
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedChef = recipes.filter(r=>r.chef_id == id);
+    console.log(selectedChef)
+    res.send(selectedChef)
+})
 app.get('/chefsprofile',(req,res)=>{
     res.send(chefsProfiles)
 })
