84704ac682e4120a63cb489a5f5baf371d48ce63
unknown <sumaiya.rrashid@gmail.comm>
2023-05-02 21:41:29 +0200

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+app.use(cors());
+const chefsProfiles = require('./data/chefprofile.json')
+
+app.get('/chefsprofile',(req,res)=>{
+    res.send(chefsProfiles)
+})
 
 app.get('/', (req, res) => {
     res.send(`hello world from dishdelight ${port}`)
